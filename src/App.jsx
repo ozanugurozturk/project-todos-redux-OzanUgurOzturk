cfc9876e18db8355fb2de2361b964025477848cb
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-05 15:36:14 +0100

    Initial guideline is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export const App = () => {
-  return <div>Find me in App.jsx!</div>;
-};
+import { Provider } from 'react-redux'
+    import { combineReducers, configureStore } from '@reduxjs/toolkit'
+    import { tasks } from './reducers/tasks'
+
+    const reducer = combineReducers({
+      tasks: tasks.reducer
+    })
+
+    const store = configureStore({ reducer })
+
+    export const App = () => {
+      return (
+        <Provider store={store}>
+          Your components can be mounted here, inside the Provider.
+        </Provider>
+      )
+    }
