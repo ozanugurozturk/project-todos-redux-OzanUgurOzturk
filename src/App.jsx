9d7d0a788e9a17c3f82a9c29601c291ef68970d3
ouozturk <ozan.ugur.ozturk@student.forsbergsskola.se>
2023-12-05 16:02:00 +0100

    ESlint is configured for
    - Recommended style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import { Provider } from 'react-redux'
-    import { combineReducers, configureStore } from '@reduxjs/toolkit'
-    import { tasks } from './reducers/tasks'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { tasks } from './reducers/tasks'
 
-    const reducer = combineReducers({
-      tasks: tasks.reducer
-    })
+const reducer = combineReducers({
+  tasks: tasks.reducer
+})
 
-    const store = configureStore({ reducer })
+const store = configureStore({ reducer })
 
-    export const App = () => {
-      return (
-        <Provider store={store}>
-          Your components can be mounted here, inside the Provider.
-        </Provider>
-      )
-    }
+export const App = () => {
+  return (
+    <Provider store={store}>
+      Your components can be mounted here, inside the Provider.
+    </Provider>
+  )
+}
